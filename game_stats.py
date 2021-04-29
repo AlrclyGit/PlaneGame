b0cd334f17df803525ce9fe31084d20dbe24df37
AlrclySuzuki <Alrcly@foxmail.com>
2021-04-29 15:54:53 +0800

    创建 Button 类

--- game_stats.py
+++ game_stats.py
@@ -7,7 +7,7 @@ class GameStats:
         # 初始化在游戏运行期间可能变化的统计信息
         self.ships_left = self.settings.ship_limit
         # 游戏活动状态标识
-        self.game_active = True
+        self.game_active = False
 
     def reset_stats(self):
         """初始化在游戏运行期间可能变化的统计信息"""
